d9f36f68931ec9a1d85e9632043035c00147d7d4
jongmin <bergi4025@gmail.com>
2026-08-08 00:09:55 +0900

    feat: 대회 결과 페이지 1차 완성

--- src/components/contests/result/AnalysisSection.tsx
+++ src/components/contests/result/AnalysisSection.tsx
@@ -4,23 +4,25 @@ import WeaknessIcon from "../../../assets/contests/result/weakness.png";
 import CorrectIcon from "../../../assets/contests/result/correct.png";
 import Image from "next/image";
 
+type Cardtype = "STRENGTH" | "WEAKNESS" | "CORRECT";
+
 interface AnalysisCardWrapperProps {
-  type: "STRENGTH" | "WEAKNESS" | "CORRECT";
+  type: Cardtype;
 }
 
 const CARD_TYPE = {
   STRENGTH: { icon: StrengthIcon, title: "강점 영역", bg: "bg-primary-100" },
-  WEAKNESS: { icon: WeaknessIcon, title: "취약 영역", bg: "bg-[#F24E4E_18]" },
+  WEAKNESS: { icon: WeaknessIcon, title: "취약 영역", bg: "bg-[#F24E4E18]" },
   CORRECT: { icon: CorrectIcon, title: "정답률", bg: "bg-primary-100" },
 };
 
 const AnalysisCardWrapper = ({ type }: AnalysisCardWrapperProps) => {
   return (
-    <div className="rounded-[14px] bg-gray-50">
+    <div className="w-55.5 rounded-[14px] bg-green-50 px-4 pt-3 pb-4">
       <p className="text-sub-text text-[12px] font-bold">
         {CARD_TYPE[type].title}
       </p>
-      <div className="mt-1 flex items-center justify-center gap-4">
+      <div className="mt-1 flex items-center justify-start gap-4">
         <div className={`${CARD_TYPE[type].bg} rounded-[50%] p-2`}>
           <Image src={CARD_TYPE[type].icon} alt={type} className="h-6 w-6" />
         </div>
@@ -35,8 +37,10 @@ const AnalysisCardWrapper = ({ type }: AnalysisCardWrapperProps) => {
 export default function AnalysisSection() {
   return (
     <DetailWrapper>
-      <p className="text-[16px] font-semibold text-black">개인 분석</p>
-      <div className="flex gap-7.5">
+      <p className="pt-4 pl-8 text-[16px] font-semibold text-black">
+        개인 분석
+      </p>
+      <div className="flex justify-center gap-7.5 py-4">
         <AnalysisCardWrapper type="STRENGTH" />
         <AnalysisCardWrapper type="WEAKNESS" />
         <AnalysisCardWrapper type="CORRECT" />
---
+++ src/components/contests/result/ResultSection.tsx
@@ -0,0 +1,52 @@
+import DetailWrapper from "./DetailWarpper";
+
+type ItemType = "CORRECT" | "INCORRECT" | "UNANSWERED";
+
+interface ReultItemProps {
+  type: ItemType;
+}
+
+const RESULT_ITEM_TYPE = {
+  CORRECT: { title: "정답", score: "10점", bg: "bg-primary-900" },
+  INCORRECT: { title: "오답", score: "0점", bg: "bg-error" },
+  UNANSWERED: { title: "미답", score: "0점", bg: "bg-disabled-text" },
+};
+
+const ResultItem = ({ type }: ReultItemProps) => {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <div
+        className={`${RESULT_ITEM_TYPE[type].bg} flex h-7.5 w-7.5 items-center justify-center rounded-[50%] text-white`}
+      >
+        1
+      </div>
+      <p>{RESULT_ITEM_TYPE[type].title}</p>
+      <p>{RESULT_ITEM_TYPE[type].score}</p>
+    </div>
+  );
+};
+
+export default function ResultSection() {
+  return (
+    <DetailWrapper>
+      <div className="flex items-center gap-3 px-8 py-7">
+        <p className="text-[16px] font-semibold text-black">문제별 결과</p>
+        <p className="text-sub-text mt-0.5 text-[10px] font-medium">
+          번호를 클릭하면 해당 문제를 바로 볼 수 있어요.
+        </p>
+      </div>
+      <div className="flex items-center justify-center gap-12 pb-11">
+        <ResultItem type="CORRECT" />
+        <ResultItem type="CORRECT" />
+        <ResultItem type="CORRECT" />
+        <ResultItem type="INCORRECT" />
+        <ResultItem type="INCORRECT" />
+        <ResultItem type="UNANSWERED" />
+        <ResultItem type="CORRECT" />
+        <ResultItem type="CORRECT" />
+        <ResultItem type="CORRECT" />
+        <ResultItem type="CORRECT" />
+      </div>
+    </DetailWrapper>
+  );
+}
